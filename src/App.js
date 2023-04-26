d0e31cca7790f7e0acd7f9a387be132f8d3a1be7
ccn <ccn@example.com>
2023-04-26 00:00:04 -0400

    test 1

--- src/App.js
+++ src/App.js
@@ -60,6 +60,7 @@ function App() {
             <Route path='/' element={<Home />}></Route>
             <Route path='/about' element={<About />}></Route>
             <Route path='/contact' element={<Contact />}></Route>
+            <Route path="*" component={Home} />
           </Routes>
         </div>
       </Router>
